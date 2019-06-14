132e8cbbf8683447d95d5cde7250fcb65810d27b
sdim-ua <ingersol@yandex.ua>
2019-06-14 10:25:40 +0300

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,25 @@
+/* 
+Играем в Дарвина
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Fish {
+
+    }
+
+    public class Animal extends Fish {
+
+    }
+
+    public class Ape extends Animal {
+
+    }
+
+    public class Human extends Ape {
+
+    }
+
+}
